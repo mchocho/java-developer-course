99a4ff2b61da0319d299afb69fa56450ddfbd6e3
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-07-06 14:26:07 +0100

    Covered logical operators

---
+++ introducing-java/LogicalFun/LogicalFun.java
@@ -0,0 +1,30 @@
+/*
+ * Logical Operators
+ *
+ * && (Logical AND)
+ * || (Logical OR)
+ * !  (Logical NOT)
+ */
+
+
+public class LogicalFun
+{
+  public static void main(String[] args)
+  {
+    boolean isRaining = false;
+    boolean isWarm    = true;
+
+    boolean combined  = isRaining && isWarm;
+
+    System.out.println("Is it raining and warm: " + combined);
+
+    combined = isRaining || isWarm;
+
+    System.out.println("Is it raining OR warm: " + combined);
+
+    combined = !isRaining;
+
+    System.out.println("Is it NOT raining outside: " + combined);
+
+  } //end main
+}
