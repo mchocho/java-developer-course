a710ee879554249d6021de225c380a6095983567
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-07-06 12:11:13 +0100

    Variables are fun

---
+++ introducing-java/VariableFun/VariableFun.java
@@ -0,0 +1,14 @@
+public class VariableFun
+{
+  public static void main(String[] args)
+  {
+    int    age  = 27;
+    String name = "Amanda";
+    final int SOME_CONST = 42;
+
+    String city = "Johannesburg";
+
+    System.out.print(name + " is " + age + '\n');
+    System.out.print("You can find me in " + city + '\n');
+  }
+}
